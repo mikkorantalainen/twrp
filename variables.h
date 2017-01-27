60eb679a2f23ff9bebfc7c500f9ee6f7f22988d8
Matt Mower <mowerm@gmail.com>
2017-01-27 16:21:01 -0600

    Mark 3.0.N1-Beta

--- variables.h
+++ variables.h
@@ -17,7 +17,7 @@
 #ifndef _VARIABLES_HEADER_
 #define _VARIABLES_HEADER_
 
-#define TW_VERSION_STR              "3.0.N0-Beta"
+#define TW_VERSION_STR              "3.0.N1-Beta"
 
 #define TW_USE_COMPRESSION_VAR      "tw_use_compression"
 #define TW_FILENAME                 "tw_filename"
